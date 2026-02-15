f5a90e4a141e378e023007edd3934d984b2f62ee
Lakshitdevjani <lakshit.bmcoder@gmail.com>
2026-02-15 20:06:07 +0530

    'push'

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get("/api/test-db", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.DB_PORT ;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
