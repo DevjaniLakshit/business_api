cc22cc7b28347b6c1d7892502b52339dd69287e8
Lakshitdevjani <lakshit.bmcoder@gmail.com>
2026-02-15 19:51:20 +0530

    'push'

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306, // IMPORTANT: set MySQL port
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306, // <-- add this line
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
